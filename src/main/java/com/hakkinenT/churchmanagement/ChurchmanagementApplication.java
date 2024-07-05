6fd40baf0500c33a8305947fee4981ade444fa8c
Tawanna <thol_16@hotmail.com>
2024-07-05 20:56:58 -0300

    Project created

---
+++ src/main/java/com/hakkinenT/churchmanagement/ChurchmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.churchmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hakkinenT/churchmanagement/ChurchmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.churchmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
