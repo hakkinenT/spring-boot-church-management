45627723a95cfe80597880cfa073e93d8c70ef20
Tawanna <thol_16@hotmail.com>
2024-07-05 21:33:41 -0300

    Church entity and database seeding created

---
+++ src/main/java/com/hakkinenT/churchmanagement/entities/Church.java
@@ -0,0 +1,84 @@
+package com.hakkinenT.churchmanagement.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_church")
+public class Church {
+    @Id
+    private String cnpj;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "mother_church_cnpj")
+    private Church motherChurch;
+
+    @OneToMany(mappedBy = "motherChurch")
+    private List<Church> congregations = new ArrayList<>();
+
+    public Church() {
+    }
+
+    public Church(String cnpj, String name, String email, Church motherChurch) {
+        this.cnpj = cnpj;
+        this.name = name;
+        this.email = email;
+        this.motherChurch = motherChurch;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Church getMotherChurch() {
+        return motherChurch;
+    }
+
+    public void setMotherChurch(Church motherChurch) {
+        this.motherChurch = motherChurch;
+    }
+
+    public List<Church> getCongregations() {
+        return congregations;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Church church = (Church) o;
+        return Objects.equals(cnpj, church.cnpj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(cnpj);
+    }
+}
